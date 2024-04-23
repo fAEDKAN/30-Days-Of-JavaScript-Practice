4013e997adae0ae97742f5ab66c23deb36fb06a5
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-04-23 04:11:54 -0300

    level 2 exercises done

---
+++ Day4/exercise2.js
@@ -0,0 +1,65 @@
+/* let grades = prompt("Ingresa tu puntuación: ");
+grades = parseInt(grades);
+switch (true) {
+  case grades >= 80 && grades <= 100:
+    alert("Tu calificación es A");
+    break;
+  case grades >= 70 && grades <= 89:
+    alert("Tu calificación es B");
+    break;
+  case grades >= 60 && grades <= 69:
+    alert("Tu calificación es C");
+    break;
+  case grades >= 50 && grades <= 59:
+    alert("Tu calificación es D");
+    break;
+  case grades >= 0 && grades <= 49:
+    alert("Tu calificación es F");
+    break;
+  default:
+    alert("No ingresaste una puntuación válida!");
+} */
+
+/* let mesUserInput = prompt("Ingresa el mes actual: ");
+let mes = mesUserInput.toLowerCase();
+switch (mes) {
+  case "septiembre":
+  case "octubre":
+  case "noviembre":
+    alert("La temporada es primavera");
+    break;
+  case "diciembre":
+  case "enero":
+  case "febrero":
+    alert("La temporada es Verano");
+    break;
+  case "marzo":
+  case "abril":
+  case "mayo":
+    alert("La temporada es Otoño");
+    break;
+  case "junio":
+  case "julio":
+  case "agosto":
+    alert("La temporada es Invierno");
+    break;
+  default:
+    alert("No ingresaste un mes válido!");
+} */
+
+/* let dayUser = prompt("Ingresa un día: ").toLowerCase();
+switch (dayUser) {
+  case "sábado":
+  case "domingo":
+    alert(`El ${dayUser} es fin de semana!`);
+    break;
+  case "lunes":
+  case "martes":
+  case "miércoles":
+  case "jueves":
+  case "viernes":
+    alert(`El ${dayUser} es día laborable :(`);
+    break;
+  default:
+    alert("Ese día no existe...");
+} */
