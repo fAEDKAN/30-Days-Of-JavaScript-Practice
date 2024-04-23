d11a7a0ceac4132d369812441fbebf6a5e7d48f6
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-04-23 02:55:13 -0300

    Wworking on level 2 exercises, the exercises to calculate the slope still need to be solved

--- Day3/exercise2.js
+++ Day3/exercise2.js
@@ -1,18 +1,19 @@
-/* let base = prompt("Ingrese la base del triángulo: ");
+/*
+let base = prompt("Ingrese la base del triángulo: ");
 let altura = prompt("Ingrese la altura del triángulo: ");
 let areaTriangulo = 0.5 * base * altura;
-alert(`El área del triángulo es ${areaTriangulo}`) */
+alert(`El área del triángulo es ${areaTriangulo}`)
 
-/* let ladoA = prompt("Ingrese el lado A del triángulo: ");
+let ladoA = prompt("Ingrese el lado A del triángulo: ");
 ladoA = parseInt(ladoA);
 let ladoB = prompt("Ingrese el lado B del triángulo: ");
 ladoB = parseInt(ladoB);
 let ladoC = prompt("Ingrese el lado C del triángulo: ");
 ladoC = parseInt(ladoC);
 let perimetroTriangulo = ladoA + ladoB + ladoC;
-alert(`El perímetro del triángulo es ${perimetroTriangulo}`); */
+alert(`El perímetro del triángulo es ${perimetroTriangulo}`);
 
-/* let largo = prompt("Ingrese el largo del rectángulo: ");
+let largo = prompt("Ingrese el largo del rectángulo: ");
 largo = parseInt(largo);
 let ancho = prompt("Ingrese el ancho del rectángulo: ");
 ancho = parseInt(ancho);
@@ -20,13 +21,68 @@ let areaRectangulo = largo * ancho;
 let perimetroRectangulo = 2 * (largo + ancho);
 alert(
   `El área del rectángulo es ${areaRectangulo} y el perímetro es ${perimetroRectangulo}`
-); */
+);
 
-/* let radio = prompt("Ingrese el radio del círculo: ");
+let radio = prompt("Ingrese el radio del círculo: ");
 radio = parseInt(radio);
 let pi = 3.14;
 let area = pi * radio * radio;
 let circunferencia = 2 * pi * radio;
-alert(`El área del círculo es ${area} y su circunferencia ${circunferencia}`); */
+alert(`El área del círculo es ${area} y su circunferencia ${circunferencia}`);
+*/
 
+/* let horas = prompt("Ingrese las horas semanales trabajadas: ");
+horas = parseInt(horas);
+let tarifaHora = prompt("Ingrese la tarifa por hora: ");
+tarifaHora = parseFloat(tarifaHora);
+let salario = horas * tarifaHora;
+alert(`Tu ganancia semanal es de $${salario}!`) */
 
+/* let nombre = prompt("Cómo te llamas?");
+if (nombre.length > 7) {
+  alert(`Tu nombre, ${nombre}, es largo!`);
+} else {
+  alert(`Tu nombre, ${nombre}, es corto!`);
+} */
+
+/* let firstName = prompt("Ingresa tu nombre");
+let lastName = prompt("Ingresa tu apellido");
+if (firstName.length < lastName.length) {
+  alert(`Tu nombre, ${firstName}, es más corto que tu apellido, ${lastName}`);
+} else {
+  alert(`Tu nombre, ${firstName}, es más largo que tu apellido, ${lastName}`);
+} */
+
+/* let myAge = 250;
+let yourAge = 30;
+console.log(`Soy ${myAge - yourAge} años mayor que vos`); */
+
+/* let yearBirth = prompt("En qué año naciste?");
+yearBirth = parseInt(yearBirth);
+const now = new Date();
+const year = now.getFullYear();
+const userAge = year - yearBirth;
+const remainingYears = 18 - userAge;
+if (userAge >= 18) {
+  alert(`Tenes ${userAge} años. Sos lo suficientemente mayor para conducir!`);
+} else {
+  alert(
+    `Tenes ${userAge} años. Vas a poder conducir en ${remainingYears} años`
+  );
+} */
+
+/* let añosUsuario = prompt("Cuántos años tenes?");
+añosUsuario = parseInt(añosUsuario);
+let segundosAño = 31536000;
+let segundosVividos = añosUsuario * segundosAño;
+alert(`Wow! Viviste ${segundosVividos} segundos!`); */
+
+/* const now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+let day = now.getDay();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+console.log(`${year}-${month}-${day} ${hour}:${minutes}`);
+console.log(`${day}-${month}-${year} ${hour}:${minutes}`);
+console.log(`${day}/${month}/${day} ${hour}:${minutes}`); */
