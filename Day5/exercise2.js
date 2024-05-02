385ec74057cd8ebdc6a1f9e806be513ac53a1c33
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-05-02 02:45:52 -0300

    working on day 5 level 2 exercises

--- Day5/exercise2.js
+++ Day5/exercise2.js
@@ -1,3 +1,24 @@
-import countries from "./countries";
-import webTechs from "./web_techs.js";
+/* import countries from "./countries";
+import webTechs from "./web_techs.js"; */
 
+/* let text =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+
+// Eliminar todas las puntuaciones
+let cleanText = text.replace(/[^\w\s]/g, "");
+
+// Convertir el string a un array
+let wordArray = cleanText.split(/\s+/);
+
+// Contar el número de palabras en el array
+let wordCount = wordArray.length;
+
+console.log("Número de palabras:", wordCount);
+console.log("Array de palabras:", wordArray); */
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+shoppingCart.unshift("Meat");
+shoppingCart.push("Sugar");
+console.log(shoppingCart);
+shoppingCart.pop("Honey");
+console.log(shoppingCart);
